acba518a8e9132e0d65da2f97b4e04161dbf5a01
Francheska Alzate <francheska.alzate@student.ateneo.edu>
2025-11-11 23:53:45 +0800

    host change

--- handacraftph/handacraftph/settings.py
+++ handacraftph/handacraftph/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG_TRUE', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['handacraftph.onrender.com']
 
 
 # Application definition
